db7379ab54afda9aae47cf746c067798c5d77126
Luca Bevilacqua <bevilacqua.luca05@gmail.com>
2025-11-10 17:07:12 +0100

    Refactor LaunchApp to dynamically instantiate views using reflection

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,10 +1,11 @@
 package it.unibo.mvc;
 
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-import it.unibo.mvc.view.DrawNumberStandardOutputView;
-import it.unibo.mvc.view.DrawNumberSwingView;
+
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Application entry-point.
@@ -24,10 +25,29 @@ public final class LaunchApp {
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws 
+            ClassNotFoundException, 
+            NoSuchMethodException, 
+            InvocationTargetException, 
+            InstantiationException, 
+            IllegalAccessException {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberStandardOutputView());
+
+        final Class<?> swingViewClass = Class.forName("it.unibo.mvc.view.DrawNumberSwingView");
+        final Class<?> stdOutputViewClass = Class.forName("it.unibo.mvc.view.DrawNumberStandardOutputView");
+        
+        final var swingViewConstructor = swingViewClass.getConstructor();
+        final var stdOutputViewConstructor = stdOutputViewClass.getConstructor();
+        
+        for (int i = 0; i < 3; i++) {
+            final DrawNumberView view = (DrawNumberView) swingViewConstructor.newInstance();
+            app.addView(view);
+        }
+        
+        for (int i = 0; i < 3; i++) {
+            final DrawNumberView view = (DrawNumberView) stdOutputViewConstructor.newInstance();
+            app.addView(view);
+        }
     }
 }
