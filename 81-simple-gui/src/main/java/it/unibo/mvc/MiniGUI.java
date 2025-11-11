9fe70903148a026a56d129dabf915f6ce6fdf8a2
Luca Bevilacqua <bevilacqua.luca05@gmail.com>
2025-11-11 14:13:50 +0100

    Add clarification comment for println in MiniGUI

--- 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -50,7 +50,7 @@ public class MiniGUI {
             final int result = randomGenerator.nextInt();
 
             resultField.setText(String.valueOf(result));
-            System.out.println(result); // NOPMD
+            System.out.println(result); // NOPMD This println was required by th exercise
         });
         bottomPanel.add(
             randomButton
